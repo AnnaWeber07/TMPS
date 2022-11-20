bf693fa36a4997bcab1f171cbd9c2f42ccd69dc3
Anna C <78998404+AnnaWeber07@users.noreply.github.com>
2022-11-20 18:59:52 +0200

    Add files via upload

---
+++ Structural/Bridge/DataObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralPatterns.Bridge
+{
+    public abstract class DataObject
+    {
+        public abstract void LatterRecord();
+        public abstract void FormerRecord();
+        public abstract void AddRecord(string name);
+        public abstract void DeleteRecord(string name);
+        public abstract string GetCurrentRecord();
+        public abstract void ShowRecord();
+        public abstract void ShowAllRecords();
+    }
+}
