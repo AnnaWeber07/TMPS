d392e0210630a7e0112af38f8a8399e4304b31c1
Anna C <78998404+AnnaWeber07@users.noreply.github.com>
2022-11-20 18:58:58 +0200

    Add files via upload

---
+++ Structural/Adapter/InternetBanking.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralPatterns.Adapter
+{
+    public class InternetBanking
+    {
+        // The databank 'legacy API'
+        public float GetCardInfo(string compound, string point)
+        {
+            // Euro
+            if (point == "E")
+            {
+                switch (compound.ToLower())
+                {
+                    case "first": return 6543.4f;
+                    case "second": return 35.5f;
+                    case "third": return -114.1f;
+                    default: return 0f;
+                }
+            }
+            // Dollar
+            else
+            {
+                switch (compound.ToLower())
+                {
+                    case "first": return 140.0f;
+                    case "second": return 86.1f;
+                    case "third": return 7.3f;
+                    default: return 0f;
+                }
+            }
+        }
+        public string GetIBAN(string compound)
+        {
+            switch (compound.ToLower())
+            {
+                case "first": return "ARG76543983298";
+                case "second": return "ARG68209487239";
+                case "third": return "ARG79505093509";
+                default: return "";
+            }
+        }
+        public double GetCardNumber(string compound)
+        {
+            switch (compound.ToLower())
+            {
+                case "first": return 363672;
+                case "second": return 579283;
+                case "third": return 111832;
+                default: return 0d;
+            }
+        }
+    }
+}
---
+++ Structural/Adapter/RichCompound.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralPatterns.Adapter
+{
+    public class ExtendedCard : Card
+    {
+        private string card;
+        private InternetBanking bank;
+        // Constructor
+        public ExtendedCard(string card)
+        {
+            this.card = card;
+        }
+        public override void Display()
+        {
+            // The Adaptee
+            bank = new InternetBanking();
+            dollarCard = bank.GetCardInfo(card, "D");
+            euroCard = bank.GetCardInfo(card, "E");
+            cardIBAN = bank.GetIBAN(card);
+            cardNumber = bank.GetCardNumber(card);
+            Console.WriteLine("Card: {0} ------ ", card);
+            Console.WriteLine(" IBAN: {0}", cardIBAN);
+            Console.WriteLine(" NUMBER : {0}", cardNumber);
+            Console.WriteLine(" EURO: {0}", euroCard);
+            Console.WriteLine(" DOLLAR: {0}", dollarCard);
+        }
+    }
+}
